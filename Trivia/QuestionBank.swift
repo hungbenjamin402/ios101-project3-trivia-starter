9f1230223f36521c8eca17255703d7b562c27afa
Manh Hung (Benjamin) Nguyen <hungbenjamin402@gmail.com>
2024-03-11 23:47:01 -0700

    Built Demo Version

--- Trivia/QuestionBank.swift
+++ Trivia/QuestionBank.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct QuestionBank {
+    let topic: String
+    let question: String
+    let pickAnswer: [String]
+    let correctAnswer: String
+    
+    static let access: [QuestionBank] = [
+        QuestionBank(topic: "General Knowledge", question: "What is the capital of France?",
+                         pickAnswer: ["Paris", "Berlin", "Madrid", "Lisbon"],
+                         correctAnswer: "Paris"),
+            QuestionBank(topic: "Science", question: "What is the chemical symbol for water?",
+                         pickAnswer: ["H2O", "CO2", "NaCl", "O2"],
+                         correctAnswer: "H2O"),
+            QuestionBank(topic: "History", question: "Who was the first President of the United States?",
+                         pickAnswer: ["Abraham Lincoln", "George Washington", "Thomas Jefferson", "John Adams"],
+                         correctAnswer: "George Washington"),
+            QuestionBank(topic: "Geography", question: "Which is the largest ocean on Earth?",
+                         pickAnswer: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+                         correctAnswer: "Pacific Ocean"),
+            QuestionBank(topic: "Sports", question: "Which country hosted the 2016 Summer Olympics?",
+                         pickAnswer: ["China", "Brazil", "Russia", "Japan"],
+                         correctAnswer: "Brazil")
+    ]
+}
--- Trivia/ViewController.swift
+++ Trivia/ViewController.swift
@@ -5,4 +5,86 @@
 //  Created by benjamin on 3/11/24.
 //
 
-import Foundation
+import UIKit
+
+class ViewController: UIViewController {
+    private var questionBank: QuestionBank? // Not Initialize => Optional
+    private var scrollingThroughQuestionBank = 0
+    private var userScore = 0
+    
+    @IBOutlet weak var currentQuestion: UILabel!
+    @IBOutlet weak var topicLabel: UILabel!
+    
+    @IBOutlet weak var questionDisplay: UITextView!
+    @IBOutlet weak var answerOneDisplay: UIButton!
+    @IBOutlet weak var answerTwoDisplay: UIButton!
+    @IBOutlet weak var answerThreeDisplay: UIButton!
+    @IBOutlet weak var answerFourDisplay: UIButton!
+    
+    @IBOutlet weak var scoreDisplay: UILabel!
+    
+    private var totalQuestions = QuestionBank.access.count
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        scoreDisplay.text = "Score: \(userScore)"
+        
+        if scrollingThroughQuestionBank < totalQuestions {
+            if !QuestionBank.access.isEmpty {
+                questionBank = QuestionBank.access[scrollingThroughQuestionBank]
+                
+                currentQuestion.text = "Question \(scrollingThroughQuestionBank)/\(totalQuestions)"
+                
+                
+                if let topic = questionBank?.topic {
+                    topicLabel.text = "Topic: \(topic)"
+                }
+                
+                questionDisplay.text = questionBank?.question
+                
+                answerOneDisplay.setTitle(questionBank?.pickAnswer[0], for: .normal)
+                answerTwoDisplay.setTitle(questionBank?.pickAnswer[1], for: .normal)
+                answerThreeDisplay.setTitle(questionBank?.pickAnswer[2], for: .normal)
+                answerFourDisplay.setTitle(questionBank?.pickAnswer[3], for: .normal)
+                
+            }
+        }
+        else {
+            scrollingThroughQuestionBank = 0
+            self.viewDidLoad()
+        }
+    }
+    
+    @IBAction func answerOneClick(_ sender: UIButton) {
+        if questionBank?.pickAnswer[0] == questionBank?.correctAnswer {
+            userScore += 1
+            scrollingThroughQuestionBank += 1
+        }
+        self.viewDidLoad()
+    }
+    
+    @IBAction func answerTwoClick(_ sender: UIButton) {
+        if questionBank?.pickAnswer[1] == questionBank?.correctAnswer {
+            userScore += 1
+            scrollingThroughQuestionBank += 1
+        }
+        self.viewDidLoad()
+    }
+    
+    @IBAction func answerThreeClick(_ sender: UIButton) {
+        if questionBank?.pickAnswer[2] == questionBank?.correctAnswer {
+            userScore += 1
+            scrollingThroughQuestionBank += 1
+        }
+        self.viewDidLoad()
+    }
+    
+    @IBAction func answerFourClick(_ sender: UIButton) {
+        if questionBank?.pickAnswer[3] == questionBank?.correctAnswer {
+            userScore += 1
+            scrollingThroughQuestionBank += 1
+        }
+        self.viewDidLoad()
+    }
+}
